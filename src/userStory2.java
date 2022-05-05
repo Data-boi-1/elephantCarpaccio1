5f8c879b6feba787c6a59f890d199a3890351795
Data-boi-1 <99009108+Data-boi-1@users.noreply.github.com>
2022-05-05 14:58:50 +0200

    lars

---
+++ src/userStory2.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class userStory2 {
+
+  Scanner sc = new Scanner(System.in);
+
+  final double taxUT = 0.0685;
+  final double taxNV = 0.08;
+  final double taxTX = 0.0625;
+  final double taxAL = 0.04;
+  final double taxCA = 0.825;
+  final int watch = -1;
+
+  final double discountRate1 = 0.03;
+  final double discountRate2 = 0.05;
+  final double discountRate3 = 0.07;
+  final double discountRate4 = 0.10;
+  final double discountRate5 = 0.15;
+
+  int price;
+
+
+  public void go() {
+/*
+    if (grossSalary <= 10_000) {
+      aTax = 0.05 * grossSalaryTaxable;
+    } else if (grossSalary <= 20_000) {
+      aTax = 0.10 * grossSalaryTaxable;
+    } else { // grossSalary > 40_000
+      aTax = 0.15 * grossSalaryTaxable;
+    }
+
+*/
+    if(price <= 5000){
+      price *
+    }
+  }
+  public static void main(String[] args) {
+  userStory2 tax = new userStory2();
+  tax.go();
+  }
+}
